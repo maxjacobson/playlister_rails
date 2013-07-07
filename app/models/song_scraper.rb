69d9d57183164518dff7975df93e9e9c666c8c72
Max Jacobson <maxwell.jacobson@gmail.com>
2013-07-07 15:46:55 -0400

    rearranged some lines whatever

--- app/models/song_scraper.rb
+++ app/models/song_scraper.rb
@@ -9,9 +9,9 @@ class SongScraper
       genre_name = filename.split(" - ")[1].split(/\[|\]/)[1]
 
       artist = Artist.find_by_name(artist_name) || Artist.create(name: artist_name)
-      song = Song.create(name: song_name)
       genre = Genre.find_by_name(genre_name) || Genre.create(name: genre_name)
-
+      song = Song.create(name: song_name)
+      
       song.add_artist(artist)
       song.add_genre(genre)
       song.save
